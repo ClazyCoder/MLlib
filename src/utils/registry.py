89dacabaabce7422bc4ea8e5e36c62dd54156c04
SGYim <xots899@naver.com>
2025-05-26 21:07:47 +0900

    fix: 레지스트리 클래스 수정
    class 이름에 종속적으로 naming 하도록 변경.

--- src/utils/registry.py
+++ src/utils/registry.py
@@ -3,24 +3,30 @@ class Registry:
         self._name = name
         self._registry = {}
 
-    def register(self, name_or_cls, cls=None):
-        if cls is None:
-            name = name_or_cls
-
+    def register(self, obj=None):
+        if obj is None:
             def decorator(func_or_cls):
+                name = func_or_cls.__name__.lower()
                 self._register(name, func_or_cls)
                 return func_or_cls
             return decorator
-        else:
-            name = name_or_cls
-            self._register(name, cls)
+
+        name = obj.__name__.lower()
+        self._register(name, obj)
 
     def _register(self, name, obj):
-        if name in self._registry:
-            raise KeyError(f"{name} already registered in {self._name}")
+        assert (
+            name not in self._registry), f"{name} already registered in {self._name}"
         self._registry[name] = obj
 
     def get(self, name):
-        if name not in self._registry:
+        obj = self._registry.get(name)
+        if obj is None:
             raise KeyError(f"{name} not found in {self._name}")
         return self._registry[name]
+
+    def __contains__(self, name):
+        return name in self._registry
+
+    def __iter__(self):
+        return iter(self._registry.items())
