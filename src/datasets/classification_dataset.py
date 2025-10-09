9cd939c6c21b128c7240f2770e3e15a03ea57488
SGYim <xots899@naver.com>
2025-10-09 12:52:51 +0900

    feat: add ClassificationDataset class for image classification

---
+++ src/datasets/classification_dataset.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision
+import torch
+
+
+class ClassificationDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx])
+        if self.transform:
+            image = self.transform(image)
+        else:
+            image = torchvision.transforms.Resize((224, 224))(image)
+            image = torchvision.transforms.ToTensor()(image)
+        return image, self.labels[idx]
