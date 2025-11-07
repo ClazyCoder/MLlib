eb20be473c6b739359adca787adf60e1d7f528a0
SGYim <xots899@naver.com>
2025-11-07 16:47:48 +0900

    refactor: update entrypoint and main script to use config name instead of path for improved clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 MODE=${1:-train}
-CONFIG_PATH=${2:-/app/configs/default.yaml}
+CONFIG_NAME=${2:-default.yaml}
 
-echo "Using config: $CONFIG_PATH"
+echo "Using config: $CONFIG_NAME"
 echo "Using mode: $MODE"
 
 if [ $MODE == "train" ]; then
     echo "[INFO] Training mode."
-    python main.py --mode train --config $CONFIG_PATH
+    python main.py --mode train --config $CONFIG_NAME
 elif [ $MODE == "test" ]; then
     echo "[INFO] Testing mode."
-    python main.py --mode test --config $CONFIG_PATH
+    python main.py --mode test --config $CONFIG_NAME
 fi
 
 echo "[INFO] Done."
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import argparse
 from src.utils.config import Config
 from src.trainers import build_trainer
+import os
+
+CONFIG_PATH = "/app/configs/"
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", type=str, default="train")
-    parser.add_argument("--config", type=str, default="config.yaml")
+    parser.add_argument("--config", type=str, default="default.yaml")
     args = parser.parse_args()
-    config = Config(args.config)
+    config = Config(os.path.join(CONFIG_PATH, args.config))
     trainer = build_trainer(config)
     if args.mode == "train":
         trainer.train()
