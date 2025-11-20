1ee9eac2b8cbbfeeaa409adf8902c6e95dab3d96
SGYim <xots899@naver.com>
2025-11-20 09:35:36 +0900

    fix: change KeyError to ValueError in Registry class for better error handling and update string representation to include only names

--- src/utils/registry.py
+++ src/utils/registry.py
@@ -28,7 +28,7 @@ class Registry:
             error_msg = f"{name} not found in {self._name} registry\n"
             error_msg += f"{self._name} registry contains the following items:\n"
             error_msg += f"{self}"
-            raise KeyError(error_msg)
+            raise ValueError(error_msg)
         return self._registry[name]
 
     def __contains__(self, name):
@@ -38,9 +38,9 @@ class Registry:
         return iter(self._registry.items())
 
     def __repr__(self):
-        repr_str = f"{self._name} registry containing the following items:\n"
-        for name, obj in self._registry.items():
-            repr_str += f"  - {name}: {obj.__class__.__name__}\n"
+        repr_str = ""
+        for name, _ in self._registry.items():
+            repr_str += f"  - {name}\n"
         return repr_str
 
 
