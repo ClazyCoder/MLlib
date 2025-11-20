7658ffe08c7bb9d95478f1b9fbbfb09045f353f9
SGYim <xots899@naver.com>
2025-11-20 10:23:57 +0900

    refactor: simplify ResNet model initializers by removing redundant config checks

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -47,54 +47,39 @@ class BaseResNet(nn.Module):
 
 @MODEL_REGISTRY.register()
 class ResNet18(BaseResNet):
-    def __init__(self, config=None):
-        if config is not None:
-            num_classes = config.get('model_config').get(
-                'num_classes', 1000)
-            pretrained = config.get('model_config').get(
-                'pretrained', True)
+    def __init__(self, config):
+        num_classes = config.get('model_config').get('num_classes', 1000)
+        pretrained = config.get('model_config').get('pretrained', True)
         super(ResNet18, self).__init__('resnet18', num_classes, pretrained)
 
 
 @MODEL_REGISTRY.register()
 class ResNet34(BaseResNet):
-    def __init__(self, config=None):
-        if config is not None:
-            num_classes = config.get('model_config').get(
-                'num_classes', 1000)
-            pretrained = config.get('model_config').get(
-                'pretrained', True)
+    def __init__(self, config):
+        num_classes = config.get('model_config').get('num_classes', 1000)
+        pretrained = config.get('model_config').get('pretrained', True)
         super(ResNet34, self).__init__('resnet34', num_classes, pretrained)
 
 
 @MODEL_REGISTRY.register()
 class ResNet50(BaseResNet):
-    def __init__(self, config=None):
-        if config is not None:
-            num_classes = config.get('model_config').get(
-                'num_classes', 1000)
-            pretrained = config.get('model_config').get(
-                'pretrained', True)
+    def __init__(self, config):
+        num_classes = config.get('model_config').get('num_classes', 1000)
+        pretrained = config.get('model_config').get('pretrained', True)
         super(ResNet50, self).__init__('resnet50', num_classes, pretrained)
 
 
 @MODEL_REGISTRY.register()
 class ResNet101(BaseResNet):
-    def __init__(self, config=None):
-        if config is not None:
-            num_classes = config.get('model_config').get(
-                'num_classes', 1000)
-            pretrained = config.get('model_config').get(
-                'pretrained', True)
+    def __init__(self, config):
+        num_classes = config.get('model_config').get('num_classes', 1000)
+        pretrained = config.get('model_config').get('pretrained', True)
         super(ResNet101, self).__init__('resnet101', num_classes, pretrained)
 
 
 @MODEL_REGISTRY.register()
 class ResNet152(BaseResNet):
-    def __init__(self, config=None):
-        if config is not None:
-            num_classes = config.get('model_config').get(
-                'num_classes', 1000)
-            pretrained = config.get('model_config').get(
-                'pretrained', True)
+    def __init__(self, config):
+        num_classes = config.get('model_config').get('num_classes', 1000)
+        pretrained = config.get('model_config').get('pretrained', True)
         super(ResNet152, self).__init__('resnet152', num_classes, pretrained)
